e9303d520cee5fafa625bea68aac64ac8aff557a
imrankazi56 <imrankazi56@gmail.com>
2022-10-19 15:56:54 +0530

    Do while Example

---
+++ 06_doWhileLoop.js
@@ -0,0 +1,14 @@
+//WAP print 1 to 5
+var count=0//Initilize
+do {
+    console.log(count);
+    count++;//Update
+    
+} while (count<=5);//condition
+
+//WAP 80 to 70
+var count =80;
+do {
+    console.log(count);
+    count--;
+} while (count>=70);
