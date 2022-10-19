a57ba17be01d85b863a74ac9266687428a08c85a
imrankazi56 <imrankazi56@gmail.com>
2022-10-19 16:01:48 +0530

    While loop

---
+++ 06_whileLoop.js
@@ -0,0 +1,24 @@
+var count =  10;
+while (count >=0) {
+console.log(count);   
+count --; 
+}
+
+//WAP Print table of 5
+var count =5;//initilization
+var square;
+while (count<=50) //condition
+{
+    console.log(count);
+    count=count+5;//updation
+
+
+}
+
+//WAP print Reverse number 10 to 1
+var count =10;
+//while (count>=0) {
+  //  console.log(count);
+    //count=count--;
+    
+//}
